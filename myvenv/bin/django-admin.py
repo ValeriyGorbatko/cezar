0fab84bb6d00acb65b3e1f54ac3cc17ddaae9f56
ValeriyGorbatko <valera71500@gmail.com>
2016-12-07 19:51:11 +0200

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/valeriy/cezar/myvenv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ shifr/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+
+class Forshifvr(models.Model):
+    text = models.TextField()
+
+
