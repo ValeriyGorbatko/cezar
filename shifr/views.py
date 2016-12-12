dcfb136333d37b80eecb6d5f60e5f6e6f09ad686
ValeriyGorbatko <valera71500@gmail.com>
2016-12-12 19:33:41 +0200

    shifr

--- shifr/views.py
+++ shifr/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render
 
+
+# def index(request):
+#     if request.POST: # проверяешь что пришел ПОСТ, тогда берез из него данные
+#         orig = request.POST['original']
+#         ste = request.POST['step']
+#     for letter in orig:
+#         global orig
+#         orig+="".join(chr(ord(letter)+ste))
+#         ste+=letter + "->" + orig + ""
 def index(request):
-    if request.POST: # проверяешь что пришел ПОСТ, тогда берез из него данные
-        orig = request.POST['original']
-        ste = request.POST['step']
-    return render(request, 'shifr/index.html', {'key': orig})
+    empty = ''
+
+    if request.POST:
+        orig = request.POST.get('original')
+        step = request.POST.get('step')
+
+        shifr = request.POST.get('shifr')
+
+        if step:
+            step=int(step)
+            if shifr == 'deshifr':
+                step = -step
+            for let in orig:
+                empty += chr(ord(let)+step)
+
+
+
+    # letter = ord('a')
+    # letter2 = ord('z')
+    # letter3 = ord('A')
+    # letter4 = ord('Z')
+    # for letter in range(97, 123):
+    #     print(chr(letter))
+    # for letter in range(65, 91):
+    #     print(chr(letter))
+
+
+
+
+
+    # ord()
+    # chr()
+
+    # orig = request.POST['original']
+    # alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    # ste = request.POST['step']
+    # cipher = ''
+    # for c in orig:
+    #     cipher += alphabet[(alphabet.index(c) + ste)]
+
+    return render(request, 'shifr/index.html', {'shifr': empty, })
