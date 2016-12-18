56498333f3b1f0acc4147228ec9ff72b2a9dc358
ValeriyGorbatko <valera71500@gmail.com>
2016-12-18 18:10:29 +0200

    completed project

--- shifr/views.py
+++ shifr/views.py
@@ -1,96 +1,50 @@
 from django.shortcuts import render
 
 
-# def index(request):
-#     if request.POST: # проверяешь что пришел ПОСТ, тогда берез из него данные
-#         orig = request.POST['original']
-#         ste = request.POST['step']
-#     for letter in orig:
-#         global orig
-#         orig+="".join(chr(ord(letter)+ste))
-#         ste+=letter + "->" + orig + ""
+class Graph(object):
+    letter = ''
+    count = 0
+
 def index(request):
+
     empty = ''
     enty = ''
     lit = ''
+    zet = ''
 
     if request.POST:
         orig = request.POST.get('original')
-        step = request.POST.get('step')
-
+        step = int(request.POST.get('step'))
         shifr = request.POST.get('shifr')
+        shifr_text = request.POST.get('shifr_text')
+
 
         if step:
-            step=int(step)
             if shifr == 'deshifr':
                 step = -step
+                orig = shifr_text
             for let in orig:
-                # empty += chr(ord(let) + step)
-                empty = ord(let)
-
                 if let.isalpha():
-                    empty = empty + step
-                    offset = 65
-                    if let.islower():
-                        offset = 97
-                        while empty < offset:
-                            empty += 26
-                        while empty > offset + 25:
-                            empty -= 26
-                        enty += chr(empty)
+                    local_step = step
+                    if let in ['z', 'Z']:
+                        local_step -= 26
+                    if let in ['a', 'A'] and step < 0:
+                        local_step += 26
+                    empty += chr(ord(let) + local_step)
+                else:
+                    empty += let
+            if shifr == 'deshifr':
+                orig, empty = empty, orig
+        else:
+            empty = orig
+        answer = []
 
-                        for lit in range(97, 123):
-                            # enty.count(chr(lit))
-                            graph = (chr(lit), enty.count(chr(lit)))
-                            print(graph)
+        for lit in range(97, 123):
+            object = Graph()
+            object.letter = chr(lit)
+            object.count = empty.count(chr(lit))
+            answer.append(object)
 
+        return render(request, 'shifr/index.html', {'shifr': empty, 'deshifr': orig, 'answer' : answer})
 
-# def chastot(enty):
-#     d = dict()
-#     for c in enty:
-#         if enty not in d:d[c] = 1
-#         else:d[c] += 1
-#     print(d)
-#                         for i in enty:
-#                             if 'a' <= i <= 'z' or 'A' <= i <= 'Z':
-#                                 word += i.lower()
-#                             else:
-#                                 if word:
-#                                     tWords += 1
-#                                     if word in words:
-#                                         words[word] = words[word] + 1
-#                                     else:
-#                                         words[word] = 1
-#                                 word = ''
-#                         # print ('total words = {0}'.format(tWords))
-#                         # print ('unique words = {0}'.format(uWords))
-#                         print(word)
-#                         print(words)
-
-
-
-
-    # letter = ord('a')
-    # letter2 = ord('z')
-    # letter3 = ord('A')
-    # letter4 = ord('Z')
-    # for letter in range(97, 123):
-    #     print(chr(letter))
-    # for letter in range(65, 91):
-    #     print(chr(letter))
-
-
-
-
-
-    # ord()
-    # chr()
-
-    # orig = request.POST['original']
-    # alphabet = list('abcdefghijklmnopqrstuvwxyz')
-    # ste = request.POST['step']
-    # cipher = ''
-    # for c in orig:
-    #     cipher += alphabet[(alphabet.index(c) + ste)]
-
-    return render(request, 'shifr/index.html', {'shifr': enty })
+    return render(request, 'shifr/index.html')
